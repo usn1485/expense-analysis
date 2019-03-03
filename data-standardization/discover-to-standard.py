e02ae5d1fc8e8f05278a58c85387f7db70cce76d
Ujwala <weboxstl@gmail.com>
2019-03-02 20:15:50 -0600

    reading csv file using arguments

--- data-standardization/discover-to-standard.py
+++ data-standardization/discover-to-standard.py
@@ -1,1 +1,21 @@
-print ("data standardization")
+import argparse
+import csv
+
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument('trasaction_file_path', help='Transaction File Path is required.')
+argument_parser.add_argument('standard_file_path', help='Standard File Path is required.')
+arguments = argument_parser.parse_args()
+
+print(arguments.standard_file_path)
+
+with open(arguments.trasaction_file_path) as csv_file:
+    csv_rows = csv.reader(csv_file , delimiter = ',')
+    line_count = 0
+    
+    for row in csv_rows:
+        if line_count == 0:
+            print(f'column names are {",".join(row)}')        
+        else:
+            print(f'row {line_count} - {",".join(row)}')
+           
+        line_count += 1
